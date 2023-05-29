4349a88f08fd8f6ca5f2b0fd8d191ba0d702cc81
Milán Balázs <milanbalazs01@gmail.com>
2023-05-29 16:58:30 +0200

    Add Init SQL script for MySQL service. (Extend the README.md file)

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# record_management_system
-University project
+# University project - Record Management System
+
+## Requirements
+
+## Deployment
+
+## Config
+
+## App - PHP
+
+## MySQL
+
+### Init state verification
+
+Enter to the MySQL container:
+ - `docker exec -it <docker_container_id> mysql -uroot -p<root_password>`
+ - `use record_management_system;`
+ - `SELECT * FROM admin_users;`
+![MySQL verification](imgs/mysql_init_verification.png)
---
+++ build/mysql/init.sql
@@ -0,0 +1,20 @@
+-- create the databases
+CREATE DATABASE IF NOT EXISTS record_management_system;
+
+USE record_management_system;
+
+CREATE TABLE IF NOT EXISTS `admin_users` (
+
+  `user_id` int(11) NOT NULL auto_increment,
+  `user_name` varchar(250)  NOT NULL default '',        
+  `first_name` varchar(250)  NOT NULL default '',
+  `last_name` varchar(250)  NOT NULL default '',
+  `password_hash` varchar(250)  NOT NULL default '',   
+   PRIMARY KEY  (`user_id`)
+);
+
+
+INSERT INTO `admin_users` (`user_name`, `first_name`, `last_name`, `password_hash`)
+    SELECT 'init_admin', 'init_admin_first_n', 'init_admin_last_n', '2fa72699dc4fc2d6138722dcc42d55cf' -- init_admin_password (MD5)
+    FROM dual
+    WHERE NOT EXISTS (SELECT * FROM `admin_users`) ;
