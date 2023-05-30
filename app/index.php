c749152b64ab6796389de5cac21c6cd7d3dc9e56
Milán Balázs <milanbalazs01@gmail.com>
2023-05-30 18:16:41 +0200

    Separate the main/login pages and add content table with some dummy data

--- app/index.php
+++ app/index.php
@@ -1,115 +1,58 @@
 <?php
-// Initialize the session
-session_start();
- 
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
-    exit;
+// Include common PHP file
+require_once "common.php";
+
+// redirect("login.php");
+
+if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['login_btn'])) {
+    redirect("login.php");
 }
- 
-// Include config file
-require_once "config.php";
- 
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = $login_err = "";
 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username!";
-    } else{
-        $username = trim($_POST["username"]);
-    }
-    
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password!";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT * FROM admin_users WHERE user_name='$username'";
-
-        if ($result = $record_management_system_db_conn->query($sql)) {
-            while ($data = $result->fetch_object()) {
-                $users[] = $data;
-            }
-        }
-
-        if (!isset($users) || empty($users)) {
-            $login_err = "Invalid username.";
-        }
-        else {
-            echo "User is found in the DB.";
-            foreach ($users as $user) {
-                if ($username == $user->user_name) {
-                    if (md5($password) == $user->password_hash){
-                        echo "Passowrd is matched";
-                    }
-                    else {
-                        $login_err = "Invalid password.";
-                    }
-                }
-            }
-        }
-                                    // Store data in session variables
-                                    //$_SESSION["loggedin"] = true;
-                                    //$_SESSION["user_id"] = $user_id;
-                                    //$_SESSION["user_name"] = $username; 
-                                    //$login_err = "Invalid username or password.";
-        
-                    // Username doesn't exist, display a generic error message
-                    // $login_err = "Invalid username or password.";
-    }
-    
-    // Close connection
-    // mysqli_close($record_management_system_db_conn);
-}
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login</title>
+    <title>Home</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="index.css">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
-    <div class="wrapper">
-        <h2 class="login_text">Login</h2>
-        <p class="please_fill_text">Please fill in your credentials to login.</p>
-
-        <?php 
-        if(!empty($login_err)){
-            echo '<div class="alert alert-danger">' . $login_err . '</div>';
-        }        
-        ?>
-
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <div class="d-flex justify-content-center">
-                    <input type="submit" class="btn btn-primary" value="Login">
-                </div>
-            </div>
-        </form>
-    </div>
+    <nav class="navbar navbar-dark bg-dark">
+    <form class="form-inline" action="index.php" method="post">
+        <input class="btn btn-success my-2 my-sm-0" type="submit" name="login_btn" value="Login" />
+    </form>
+    </nav>
+    <table class="table table-dark">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
 </body>
-</html>
---
+++ app/login.php
@@ -0,0 +1,118 @@
+<?php
+// Initialize the session
+session_start();
+
+// Include common PHP file
+require_once "common.php";
+
+// Check if the user is already logged in, if yes then redirect him to index page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    redirect("index.php");
+}
+ 
+// Include config PHP file
+require_once "config.php";
+
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username!";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password!";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT * FROM admin_users WHERE user_name='$username'";
+
+        if ($result = $record_management_system_db_conn->query($sql)) {
+            while ($data = $result->fetch_object()) {
+                $users[] = $data;
+            }
+        }
+
+        if (!isset($users) || empty($users)) {
+            $login_err = "Invalid username.";
+        }
+        else {
+            echo "User is found in the DB.";
+            foreach ($users as $user) {
+                if ($username == $user->user_name) {
+                    if (md5($password) == $user->password_hash){
+                        echo "Passowrd is matched";
+                    }
+                    else {
+                        $login_err = "Invalid password.";
+                    }
+                }
+            }
+        }
+                                    // Store data in session variables
+                                    //$_SESSION["loggedin"] = true;
+                                    //$_SESSION["user_id"] = $user_id;
+                                    //$_SESSION["user_name"] = $username; 
+                                    //$login_err = "Invalid username or password.";
+        
+                    // Username doesn't exist, display a generic error message
+                    // $login_err = "Invalid username or password.";
+    }
+    
+    // Close connection
+    // mysqli_close($record_management_system_db_conn);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="login.css">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <div class="wrapper">
+        <h2 class="login_text">Login</h2>
+        <p class="please_fill_text">Please fill in your credentials to login.</p>
+
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        }        
+        ?>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <div class="d-flex justify-content-center">
+                    <input type="submit" class="btn btn-primary" value="Login">
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
